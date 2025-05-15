50127136985a98732f901c3640fdab040cae13e2
Talal <talalaslam15@gmail.com>
2025-05-15 22:41:10 +0500

    Update users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -11,11 +11,28 @@ export class UsersService {
   }
 
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 
   findOne(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 
   update(id: string, data: Prisma.UserUpdateInput) {
