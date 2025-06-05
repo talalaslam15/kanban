52bdfd581ced0269f6e8300143a82a50f9b40465
Talal <talalaslam15@gmail.com>
2025-06-05 23:29:29 +0500

    Update AuthLayout.tsx

--- frontend/src/auth/AuthLayout.tsx
+++ frontend/src/auth/AuthLayout.tsx
@@ -1,7 +1,9 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { useTheme } from "@/components/theme-provider";
 import { Outlet } from "react-router";
 
 export const AuthLayout = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex min-h-screen max-h-screen">
       <div className="p-8 flex-1">
@@ -9,6 +11,7 @@ export const AuthLayout = () => {
       </div>
       <div className="p-8 flex-1">
         <img
+          style={{ filter: theme === "dark" ? "brightness(0.5)" : "none" }}
           src="https://images.pexels.com/photos/6592358/pexels-photo-6592358.jpeg/"
           alt="Kanban Board"
           className="rounded-lg shadow-lg w-full h-full object-cover"
